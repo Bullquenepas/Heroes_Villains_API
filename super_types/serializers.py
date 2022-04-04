b0fd72734fab052db15ab710681dc80af5afdc99
Christopher Bonilla <Bullquenepas>
2022-04-04 16:32:12 -0400

    Added rest_framework, super_types, supers to INSTALLED_APPS in settings.py + Created Class SuperType and Assigned Property + Created serializers.py for Both Apps.

---
+++ super_types/serializers.py
@@ -0,0 +1,4 @@
+# Each app should have a “serializers.py” file (created manually)
+# In this file we define serializer classes for each model in that same app
+# The serializer class is responsible for converting JSON objects into the model type as well as converting objects into JSON
+
